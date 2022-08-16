9795444e80b8774a8cb742c7c779967e58d63277
GNikroo <gina.nikroo@gmail.com>
2022-08-16 07:35:36 +0000

    Add admin post and comment capabilities

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,22 @@
+'''Admin for No Sweat fitforum'''
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    '''PostAdmin Class'''
+    list_display = (
+        'title', 'slug', 'status', 'author', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author', 'created_on')
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    '''CommentAdmin Class'''
+    list_display = ('name', 'post', 'body', 'created_on')
+    search_fields = ['name', 'email', 'body']
