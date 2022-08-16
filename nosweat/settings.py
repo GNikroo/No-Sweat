150b90287f87c4acb39a25e935ad5b8f1881991d
GNikroo <gina.nikroo@gmail.com>
2022-08-16 09:02:06 +0000

    Add authorization and style html

--- nosweat/settings.py
+++ nosweat/settings.py
@@ -19,7 +19,11 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+SITE_ID = 1
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -45,6 +49,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount', 
     'forum',
 ]
 
